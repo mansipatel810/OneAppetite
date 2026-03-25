a7fdcf6a14a3d662c04ac68e7277ecaaefd12aef
Patel <2479385@cognizant.com>
2026-03-25 11:48:07 +0530

    fix: include actual frontend files instead of submodule

---
+++ Frontend/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; // 1. ADD THIS IMPORT
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideHttpClient() // 2. ADD THIS PROVIDER
+  ]
+};
