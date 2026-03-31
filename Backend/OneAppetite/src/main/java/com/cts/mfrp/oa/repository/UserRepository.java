aa4d5c607f156eafa2d05da18da8ed5b118fdf73
sanje23 <rameshsanje23@gmail.com>
2026-03-31 17:08:48 +0530

    added feature vendor listing

--- Backend/OneAppetite/src/main/java/com/cts/mfrp/oa/repository/UserRepository.java
+++ Backend/OneAppetite/src/main/java/com/cts/mfrp/oa/repository/UserRepository.java
@@ -1,13 +1,19 @@
 package com.cts.mfrp.oa.repository;
 
+import com.cts.mfrp.oa.model.Building;
+import com.cts.mfrp.oa.model.Role;
 import com.cts.mfrp.oa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    // Custom query to fetch vendors by building
+    List<User> findByBuildingAndRole(Building building, Role role);
 }
